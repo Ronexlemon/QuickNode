d8cc97384a1244650818aeb4f80a81249cec3460
RonexLemon <ronexondimu@gmail.com>
2022-11-04 13:19:14 +0300

    deploy contract

--- quicknode/backend/contracts/OnlineBook.sol
+++ quicknode/backend/contracts/OnlineBook.sol
@@ -2,61 +2,66 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract OnlineBookStore is Ownable{
-    struct BookStore{
+
+contract OnlineBookStore is Ownable {
+    struct BookStore {
         address payable owner;
-       string  bookname;
-       string bookDescription;
-       uint bookprice;
-       uint bookId;
-       bool sold;
-
-
+        string bookname;
+        string bookDescription;
+        uint bookprice;
+        uint bookId;
+        bool sold;
+        //BookContractAddress 0x1D68e74456fA88F1d54298CBAba61acbBcd0D7FD
     }
-    mapping(uint256 => BookStore)public bookstore;
-    uint bookindex=0 ;
-    mapping(uint => bool)private _exists;
-    modifier exist(uint index){
-        require(_exists[index],"query only books that exists");
+    mapping(uint256 => BookStore) public bookstore;
+    uint bookindex = 0;
+    mapping(uint => bool) private _exists;
+    modifier exist(uint index) {
+        require(_exists[index], "query only books that exists");
         _;
     }
 
-    function addBook(string calldata _bookname,string calldata _bookdescription,uint _bookprice)public{
-        BookStore storage  bookArchive = bookstore[bookindex];
-         bookArchive.owner = payable(msg.sender);
+    function addBook(
+        string calldata _bookname,
+        string calldata _bookdescription,
+        uint _bookprice
+    ) public {
+        BookStore storage bookArchive = bookstore[bookindex];
+        bookArchive.owner = payable(msg.sender);
         bookArchive.bookname = _bookname;
-         bookArchive.bookDescription = _bookdescription;
-         bookArchive.bookprice= _bookprice;
-          bookArchive.bookId = bookindex;
-           bookArchive.sold = false;
-             _exists[bookindex] = true;
+        bookArchive.bookDescription = _bookdescription;
+        bookArchive.bookprice = _bookprice;
+        bookArchive.bookId = bookindex;
+        bookArchive.sold = false;
+        _exists[bookindex] = true;
 
-           bookindex = bookindex +1;
-         
+        bookindex = bookindex + 1;
     }
-    function getAllBooks()public view returns(BookStore[] memory props){
+
+    function getAllBooks() public view returns (BookStore[] memory props) {
         props = new BookStore[](bookindex);
-        for(uint index=0; index< bookindex; index++){
+        for (uint index = 0; index < bookindex; index++) {
             props[index] = bookstore[index];
         }
-
-
     }
-    function removeBook(uint _bookId)public onlyOwner exist(_bookId) {
-    uint newbookindex = bookindex - 1;
-    bookindex = newbookindex;
-    _exists[newbookindex] = false;
-    bookstore[_bookId] = bookstore[newbookindex];
-    delete  bookstore[newbookindex];
-    }
-    
-    function buyBook(uint _bookId)public payable{
-        require(bookstore[_bookId].owner != msg.sender,"Please you can not buy your own book ");
-        bookstore[_bookId].owner.transfer( bookstore[_bookId].bookprice );
-        
 
+    function removeBook(uint _bookId) public onlyOwner exist(_bookId) {
+        uint newbookindex = bookindex - 1;
+        bookindex = newbookindex;
+        _exists[newbookindex] = false;
+        bookstore[_bookId] = bookstore[newbookindex];
+        delete bookstore[newbookindex];
     }
-    function getTotalNumberOfBooks()public view returns(uint){
+
+    function buyBook(uint _bookId) public payable {
+        require(
+            bookstore[_bookId].owner != msg.sender,
+            "Please you can not buy your own book "
+        );
+        bookstore[_bookId].owner.transfer(bookstore[_bookId].bookprice);
+    }
+
+    function getTotalNumberOfBooks() public view returns (uint) {
         return bookindex;
     }
 }
